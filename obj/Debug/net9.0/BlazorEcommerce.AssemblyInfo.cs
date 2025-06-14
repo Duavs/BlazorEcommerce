ad67173fa6f8a0ad643880914dd54a1a03a2e3c2
John Duavis <paulduavis09@gmail.com>
2025-06-14 23:17:51 +0800

    update BindProp.razor to add product table with editable fields and summary display; update AssemblyInformationalVersion

--- obj/Debug/net9.0/BlazorEcommerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c747be94bbbacc74da238a441a3c946a238fd7f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c3c4d37b1a5f8c06f7370413d4eccad5a64022")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
