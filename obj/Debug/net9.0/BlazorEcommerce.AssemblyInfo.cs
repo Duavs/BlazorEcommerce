07ff47be2a4d7d518e022da4ff3619cb61e3d5f5
John Duavis <paulduavis09@gmail.com>
2025-06-22 23:04:05 +0800

    update AssemblyInformationalVersion to reflect latest commit hash

--- obj/Debug/net9.0/BlazorEcommerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a193560f24dc7af6ac8e1505384de45e2a45f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c691240b60b42833b4cdfd6b200adbeffa1ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
